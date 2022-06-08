d2ce329e3f41d3876b1e26611391fe1c0740ad96
BaxtiyorErkinov <baxa10th@gmail.com>
2022-06-08 21:21:21 +0500

    s

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-function pipeFix(n) {
-  const res = [];
-  for(let i = n[0]; i <= n[n.length - 1]; i++) {
-    res.push(i)
-  }
-  return res
-}
-const res = pipeFix([1, 2, 3, 4, 7, 10]);
-console.log(res);
-=======
 function news() {
-  return "new"
+  return "new";
 }
->>>>>>> new
