ed147b8aca8b4e5862ae11ef897ebd63a27a2678
BaxtiyorErkinov <baxa10th@gmail.com>
2022-06-24 10:55:06 +0500

    class

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-function news() {
-  return "new"
+class Component {
+	constructor(el) {
+		this.el = document.getElementById(el)
+	}
 }
+
+class Box extends Component {
+	constructor(options) {
+		super(options.el)
+		this.el.style.width = options.width + "px"
+		this.el.style.height = options.height + "px"
+		this.el.style.background = options.color
+
+	}
+}
+
+const box = new Box({
+	el: "box",
+	width: 100,
+	height: 100,
+	color: "red"
+})
